f610c60b39b3e690525cdffa4a9e487cf6572e16
Tony <suntaoas2014@gmail.com>
2016-04-18 19:53:56 -0400

    realize a demo for change language display

--- app/src/main/java/com/foodmap/foodmap/MainActivity.java
+++ app/src/main/java/com/foodmap/foodmap/MainActivity.java
@@ -2,14 +2,26 @@ package com.foodmap.foodmap;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button bt1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bt1 = (Button) findViewById(R.id.bt1);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bt1.setText(getString(R.string.cn_buttonTest1));
+            }
+        });
     }
+
+
 
 
 }

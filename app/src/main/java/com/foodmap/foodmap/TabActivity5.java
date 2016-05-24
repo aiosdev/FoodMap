a6e247293b06c6ecdf2a9fe1ddfd555bc3717c8c
Tony <suntaoas2014@gmail.com>
2016-05-24 16:44:11 -0400

    add the authority of google play google service , not finish.

--- app/src/main/java/com/foodmap/foodmap/TabActivity5.java
+++ app/src/main/java/com/foodmap/foodmap/TabActivity5.java
@@ -1,9 +1,13 @@
 package com.foodmap.foodmap;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -12,59 +16,26 @@ public class TabActivity5 extends Activity {
 
 	public TextView textView;
 
-
-	private static Method sGetOpenSourceSoftwareLicenseInfo;
-	private static final String THIS_CLASS_PACKAGE = "com.google.android.gms.common.GooglePlayServicesUtil";
-
-	static {
-
-		Class<?> googlePlayServicesUtilClass = null;
-		try {
-			googlePlayServicesUtilClass = Class.forName(THIS_CLASS_PACKAGE);
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-
-		try {
-			sGetOpenSourceSoftwareLicenseInfo = googlePlayServicesUtilClass.getMethod(
-					"getOpenSourceSoftwareLicenseInfo", Context.class);
-		} catch (NoSuchMethodException e) {
-			e.printStackTrace();
-		}
-
-	}
-
-		/**
-		 * Returns the open source software license information for the Google Play services application, or null if
-		 * Google Play services is not available on this device.
-		 *
-		 * @param context
-		 * @return
-		 * @throws InvocationTargetException
-		 * @throws IllegalAccessException
-		 */
-	public static String getOpenSourceSoftwareLicenseInfo(Context context)
-			throws InvocationTargetException, IllegalAccessException {
-		if (sGetOpenSourceSoftwareLicenseInfo == null) {
-			return null;
-		}
-
-		return (String) sGetOpenSourceSoftwareLicenseInfo.invoke(null, context);
-	}
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.tab_5);
 
 		textView = (TextView) findViewById(R.id.tv_5);
-		try {
-			textView.setText((String)sGetOpenSourceSoftwareLicenseInfo.invoke(null, this));
-			String test = (String)sGetOpenSourceSoftwareLicenseInfo.invoke(null, this);
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
-			e.printStackTrace();
+		GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+		int resultCode = apiAvailability.isGooglePlayServicesAvailable(this);
+		String info = GoogleApiAvailability.getInstance().getOpenSourceSoftwareLicenseInfo(getApplicationContext());
+		System.out.println("wwwwwwwwwwwww"+resultCode+","+info);
+
+		if (resultCode == ConnectionResult.SUCCESS) {
+			//AlertDialog.Builder builder = new AlertDialog.Builder(TabActivity5.this);
+			//builder.setTitle("About");
+			//builder.setMessage(apiAvailability.getOpenSourceSoftwareLicenseInfo(this));
+			//builder.setPositiveButton("OK", null);
+			//builder.show();
+			info = apiAvailability.getOpenSourceSoftwareLicenseInfo(this);
+			textView.setText(info);
+
 		}
 	}
 
